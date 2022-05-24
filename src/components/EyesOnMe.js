a3f13c29f4033caa2aec14280ac668ea554daed5
Naftali Kulik <nkulik1994@gmail.com>
2022-05-24 13:17:48 -0400

    commit tree 5e3498af85c33944f32940e51c2f7faa15d56388

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+
+    function handleEvent(e) {
+        e._reactName === 'onFocus' ? console.log('Good!') : console.log('Hey! Eyes on me!')
+    }
+
+    return <button onFocus={handleEvent} onBlur={handleEvent}>Eyes on me</button>
+}
+
+export default EyesOnMe
